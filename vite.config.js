851183796b25e6170ccab05a0102950a3c8fb778
jeronimo.aranda <jeronimo.aranda@gmail.com>
2026-02-09 14:24:55 -0300

    Agregando configuración base y scripts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Seguimiento-Obra-BDT/', // <--- AGREGA ESTA LÍNEA (con las barras al inicio y final)
 })
